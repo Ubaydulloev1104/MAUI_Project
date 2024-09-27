8339b00e7cbe22be564ad9781e1c4db1a998e607
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-09-27 22:42:06 +0500

    fix 1

---
+++ identity/src/Identity_Application/Features/Role/RoleProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Identity_Application.Contracts.ApplicationRoles.Responses;
+using Identity_Domain.Entities;
+
+namespace Identity_Application.Features.Role;
+
+public class RoleProfile : Profile
+{
+    public RoleProfile()
+    {
+        CreateMap<ApplicationRole, RoleNameResponse>();
+
+    }
+}
