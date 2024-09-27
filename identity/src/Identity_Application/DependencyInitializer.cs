fe8cc9d853bc628377f7cd0d1d6e7406b8fa6cae
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-09-27 23:10:52 +0500

    Add DependencyInitializer

---
+++ identity/src/Identity_Application/DependencyInitializer.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MediatR;
+using Identity_Application.Common.Interfaces.Services;
+using Identity_Application.Features.Users;
+using Identity_Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Identity_Application;
+
+public static class DependencyInitializer
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        services.AddOptions();
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+
+        services.AddAutoMapper(typeof(UsersProfile).Assembly);
+
+        Assembly assem = Assembly.GetExecutingAssembly();
+        services.AddValidatorsFromAssembly(assem);
+
+        services.AddScoped<IJwtTokenService, JwtTokenService>();
+    }
+}
