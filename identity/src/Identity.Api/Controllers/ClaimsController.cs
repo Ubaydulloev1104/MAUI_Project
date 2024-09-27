3000e05e3f7b6f22b050aa4ca1740ea0c3d18154
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-09-27 23:15:17 +0500

    Add Controller

---
+++ identity/src/Identity.Api/Controllers/ClaimsController.cs
@@ -0,0 +1,69 @@
+﻿using Identity_Application.Contracts.Claim.Commands;
+using Identity_Application.Contracts.Claim.Queries;
+using Identity_Application.Contracts.Claim.Responses;
+using Identity_Infrastructure.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Api.Controllers;
+
+[ApiController]
+[Route("/api/[controller]/")]
+[Authorize(ApplicationPolicies.Administrator)]
+public class ClaimsController : ControllerBase
+{
+    private readonly ISender _mediator;
+
+    public ClaimsController(ISender mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateClaimCommand request)
+    {
+        var response = await _mediator.Send(request);
+        return Ok();
+    }
+
+
+    [HttpPut("{slug}")]
+    public IActionResult Put([FromBody] UpdateClaimCommand command, string slug)
+    {
+
+        if (slug != command.Slug)
+        {
+            return BadRequest();
+        }
+        var response = _mediator.Send(command);
+        return Ok();
+
+    }
+
+    [HttpDelete("{slug}")]
+    public async Task<IActionResult> Delete(string slug)
+    {
+        var response = await _mediator.Send(new DeleteClaimCommand
+        {
+            Slug = slug
+        });
+        return Ok();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get([FromQuery] GetAllQuery query)
+    {
+
+        List<UserClaimsResponse> response = await _mediator.Send(query);
+        return Ok(response);
+    }
+
+    [HttpGet("{slug}")]
+    public async Task<IActionResult> Get(string slug)
+    {
+
+        var response = await _mediator.Send(new GetBySlugQuery { Slug = slug });
+        return Ok(response);
+    }
+}
---
+++ identity/src/Identity.Api/Controllers/ProfileController.cs
@@ -0,0 +1,34 @@
+﻿using Identity_Application.Contracts.Profile.Commands.UpdateProfile;
+using Identity_Application.Contracts.Profile.Queries;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+
+public class ProfileController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ProfileController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    [HttpPut]
+    public async Task<IActionResult> UpdateProfile([FromBody] UpdateProfileCommand command)
+    {
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetProfileByUserName([FromQuery] string userName = null)
+    {
+        var result = await _mediator.Send(new GetPofileQuery { UserName = userName });
+        return Ok(result);
+    }
+    
+}
