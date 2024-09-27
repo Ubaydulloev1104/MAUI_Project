515d66c89c5b5766707306dd42a56e6c1eae0a5e
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-09-27 23:16:37 +0500

    12

---
+++ identity/src/Identity.Api/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Identity_Infrastructure.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+using TSR_Accoun_Application.Contracts.User.Queries.CheckUserDetails;
+using TSR_Accoun_Application.Contracts.User.Queries;
+
+namespace Identity.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize(ApplicationPolicies.Administrator)]
+public class UserController : ControllerBase
+{
+    private readonly ISender _mediator;
+
+    public UserController(ISender mediator)
+    {
+        _mediator = mediator;
+    }
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var users = await _mediator.Send(new GetAllUsersQuery());
+        return Ok(users);
+    }
+    [HttpGet("{userName}")]
+    public async Task<IActionResult> Get(string userName)
+    {
+        var query = new GetUserByUsernameQuery { UserName = userName };
+        var user = await _mediator.Send(query);
+        return Ok(user);
+    }
+
+
+    [HttpGet("CheckUserDetails/{userName}/{phoneNumber}/{email}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> CheckUserDetails([FromRoute] string userName, [FromRoute] string phoneNumber, [FromRoute] string email)
+    {
+        var result = await _mediator.Send(new CheckUserDetailsQuery() { UserName = userName, PhoneNumber = phoneNumber, Email = email });
+        return Ok(result);
+    }
+}
