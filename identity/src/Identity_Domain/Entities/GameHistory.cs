76ff760d14c2c18a79e6ada8c821867b45562902
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-10-15 22:05:04 +0500

    1

---
+++ identity/src/Identity_Domain/Entities/GameHistory.cs
@@ -0,0 +1,8 @@
+﻿namespace Identity_Domain.Entities;
+
+public class GameHistory
+{
+    public int Id { get; set; }
+    public int Name { get; set; }
+    public string Description { get; set; }
+}
