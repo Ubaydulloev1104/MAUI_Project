ff94219bf31763057736c757bdc576404fdf8916
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-10-25 22:10:23 +0500

    fix UserScores

--- identity/src/Identity_Application/Common/Interfaces/DbContexts/IApplicationDbContext.cs
+++ identity/src/Identity_Application/Common/Interfaces/DbContexts/IApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Identity_Domain.Entities;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Identity_Application.Common.Interfaces.DbContexts;
@@ -12,5 +13,6 @@ public interface IApplicationDbContext
     public DbSet<ApplicationUserRole> UserRoles { get; set; }
     public DbSet<ApplicationRole> Roles { get; set; }
     public DbSet<IdentityRoleClaim<Guid>> RoleClaims { get; set; }
+    public DbSet<UserScores> UserScores { get; set; }
     
 }
