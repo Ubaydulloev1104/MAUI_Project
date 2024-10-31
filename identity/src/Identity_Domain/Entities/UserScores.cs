c2b153c658e0c978b9ecf77b6a04337de4df966a
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-10-31 22:03:03 +0500

    fix

--- identity/src/Identity_Domain/Entities/UserScores.cs
+++ identity/src/Identity_Domain/Entities/UserScores.cs
@@ -1,9 +1,10 @@
 ﻿namespace Identity_Domain.Entities;
 public class UserScores
 {
-    public int Id { get; set; } 
+    public string Slug { get; set; }
+    public int Id { get; set; }
     public int Score { get; set; }
-    public int Points { get; set; }
+
     public string IncorrectQuestion { get; set; }
     public DateTime LastUpdated { get; set; }
     public Guid UserId { get; set; }
