3b25bafbd18c27f5b35898566df7320243b33b78
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-11-01 21:24:54 +0500

    fix

--- identity/src/Identity.Api/Program.cs
+++ identity/src/Identity.Api/Program.cs
@@ -1,25 +1,41 @@
+using FluentValidation.AspNetCore;
+using Identity.Api.Filters;
+using Identity_Application;
+using Identity_Infrastructure;
+using Identity_Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<ApiExceptionFilterAttribute>();
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI();
+using (var scope = app.Services.CreateScope())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var initialiser = scope.ServiceProvider.GetRequiredService<DbMigration>();
+    await initialiser.InitialiseAsync();
 }
+var applicationDbContextInitializer = app.Services.CreateAsyncScope().ServiceProvider.GetRequiredService<ApplicationDbContextInitializer>();
+await applicationDbContextInitializer.SeedAsync();
+
+app.UseCors("CORS_POLICY");
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
