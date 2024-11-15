d296265845163a772a7197a850c16dabb81c599f
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-11-15 21:21:37 +0500

    fix

--- identity/src/Identity.Api/Controllers/UserScoresControllrer.cs
+++ identity/src/Identity.Api/Controllers/UserScoresControllrer.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Identity_Application.Contracts.Profile.Commands.UpdateProfile;
+using Identity_Application.Contracts.Profile.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Identity.Api.Controllers;
@@ -7,5 +10,17 @@ namespace Identity.Api.Controllers;
 [Authorize]
 public class UserScoresControllrer:ControllerBase
 {
+    private readonly IMediator _mediator;
 
+    public UserScoresControllrer(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+   
+    [HttpGet]
+    public async Task<IActionResult> GetUserScores([FromQuery] string userName = null)
+    {
+        var result = await _mediator.Send(new GetPofileQuery { UserName = userName });
+        return Ok(result);
+    }
 }
