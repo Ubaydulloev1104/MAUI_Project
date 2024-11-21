bba79b38d76c5a3f7ec5e7f1f2979a25a8db6a9a
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-11-21 22:02:34 +0500

    fix update

---
+++ identity/src/Identity_Application.Contracts/UserScores/Commands/Update/UpdateUserScoreDetailCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Identity_Application.Contracts.UserScores.Commands.Update;
+
+public class UpdateUserScoreDetailCommand : IRequest<Guid>
+{
+    public string Slug { get; set; }
+    public int Id { get; set; }
+    public int Score { get; set; }
+    public string IncorrectQuestion { get; set; }
+    public DateTime LastUpdated { get; set; }
+}
---
+++ identity/src/Identity_Application.Contracts/UserScores/Commands/Update/UpdateUserScoreDetailCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Identity_Application.Contracts.UserScores.Commands.Update;
+
+public class UpdateUserScoreDetailCommandValidator : AbstractValidator<UpdateUserScoreDetailCommand>
+{
+    public UpdateUserScoreDetailCommandValidator()
+    {
+        RuleFor(e => e.Id).NotEmpty();
+        RuleFor(e => e.LastUpdated).NotEmpty();
+        RuleFor(e => e.IncorrectQuestion).NotEmpty();
+        RuleFor(e => e.Score).NotEmpty();
+    }
+}
