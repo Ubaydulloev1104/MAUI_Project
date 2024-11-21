bd3a9a265e77990859a4073c2cf1b2a17653e677
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-11-21 21:57:47 +0500

    fix delete

---
+++ identity/src/Identity_Application.Contracts/UserScores/Commands/Delete/DeleteUserScoreCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Identity_Application.Contracts.UserScores.Commands.Delete;
+
+public class DeleteUserScoreCommand : IRequest<bool>
+{
+    public Guid Id { get; set; }
+}
---
+++ identity/src/Identity_Application.Contracts/UserScores/Commands/Delete/DeleteUserScoreCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Identity_Application.Contracts.UserScores.Commands.Delete;
+
+public class DeleteUserScoreCommandValidator : AbstractValidator<DeleteUserScoreCommand>
+{
+    public DeleteUserScoreCommandValidator()
+    {
+        RuleFor(e => e.Id).NotEmpty();
+    }
+}
