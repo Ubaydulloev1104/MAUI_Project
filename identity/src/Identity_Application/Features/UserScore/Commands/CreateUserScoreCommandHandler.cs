57d5ea297f49c3b7225f78dc9fe59b3645b13044
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-11-29 22:08:47 +0500

    1

--- identity/src/Identity_Application/Features/UserScore/Commands/CreateUserScoreCommandHandler.cs
+++ identity/src/Identity_Application/Features/UserScore/Commands/CreateUserScoreCommandHandler.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Identity_Application.Features.UserScores.Commands
+namespace Identity_Application.Features.UserScore.Commands
 {
     public class CreateUserScoreCommandHandler
     {
