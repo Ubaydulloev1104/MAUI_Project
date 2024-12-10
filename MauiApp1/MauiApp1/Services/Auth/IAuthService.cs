c2c9bd9f9633d1a1960c2e339ce850d21197eccc
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-12-10 22:50:59 +0500

    1

---
+++ MauiApp1/MauiApp1/Services/Auth/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp1.Services.Auth
+{
+    public interface IAuthService
+    {
+        Task<string> RegisterUserAsync(RegisterUserCommand command);
+        Task<string> LoginUserAsync(LoginUserCommand command, bool newRegister = false);
+        Task<HttpResponseMessage> ChangePassword(ChangePasswordUserCommand command);
+        Task<HttpResponseMessage> CheckUserName(string userName);
+        Task<HttpResponseMessage> CheckUserDetails(CheckUserDetailsQuery checkUserDetailsQuery);
+    }
+}
