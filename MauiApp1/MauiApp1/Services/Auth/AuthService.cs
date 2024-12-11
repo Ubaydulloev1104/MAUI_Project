032af563a0b73c94dfa91f6266aedcbef8ec8720
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-12-11 21:19:59 +0500

    Add IUSerProfi

---
+++ MauiApp1/MauiApp1/Services/Auth/AuthService.cs
@@ -0,0 +1,6 @@
+﻿namespace MauiApp1.Services.Auth
+{
+    public class AuthService
+    {
+    }
+}
---
+++ MauiApp1/MauiApp1/Services/Profile/IUserProfileService.cs
@@ -0,0 +1,26 @@
+﻿using Identity_Application.Contracts.Profile.Commands.UpdateProfile;
+using Identity_Application.Contracts.Profile.Responses;
+using Identity_Application.Contracts.UserScores.Commands.Create;
+using Identity_Application.Contracts.UserScores.Commands.Update;
+using Identity_Application.Contracts.UserScores.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Services.Profile
+{
+    public interface IUserProfileService
+    {
+        Task<UserProfileResponse> Get(string userName = null);
+        Task<string> Update(UpdateProfileCommand command);
+        Task<List<UserScoresResponse>> GetEducationsByUser();
+        Task<List<UserScoresResponse>> GetAllEducations();
+        Task<HttpResponseMessage> CreateEducationAsуnc(CreateUserScoreDetailCommand command);
+        Task<HttpResponseMessage> UpdateEducationAsync(UpdateUserScoreDetailCommand command);
+        Task<HttpResponseMessage> DeleteEducationAsync(Guid id);
+
+        Task<bool> SendConfirmationCode(string phoneNumber);
+    }
+}
