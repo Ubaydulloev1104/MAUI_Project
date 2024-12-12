22087180d8174bc1f125455759b17b45462298a2
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-12-12 21:14:00 +0500

    1

---
+++ MauiApp1/MauiApp1/DB/Entities/IncorrectAnswer.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp1.DB.Entities
+{
+    public class IncorrectAnswer
+    {
+        public Guid Id { get; set; }
+        public string Problem { get; set; }
+        public int CorrectAnswer { get; set; }
+        public int UserAnswer { get; set; }
+    }
+
+}
--- MauiApp1/MauiApp1/Services/Profile/UserProfileService.cs
+++ MauiApp1/MauiApp1/Services/Profile/UserProfileService.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Identity_Application.Contracts.Profile.Commands.UpdateProfile;
+using Identity_Application.Contracts.Profile.Responses;
+using Identity_Application.Contracts.UserScores.Commands.Create;
+using Identity_Application.Contracts.UserScores.Commands.Update;
+using Identity_Application.Contracts.UserScores.Response;
 
 namespace MauiApp1.Services.Profile
 {
     public class UserProfileService : IUserProfileService
     {
+        public Task<HttpResponseMessage> CreateEducationAsуnc(CreateUserScoreDetailCommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> DeleteEducationAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserProfileResponse> Get(string userName = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserScoresResponse>> GetAllEducations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserScoresResponse>> GetEducationsByUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SendConfirmationCode(string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> Update(UpdateProfileCommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> UpdateEducationAsync(UpdateUserScoreDetailCommand command)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
