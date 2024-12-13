16abb80cb78a4bd1672a73dcdbdeafb405340301
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-12-13 21:25:36 +0500

    1

--- MauiApp1/MauiApp1/IdentityHttpClient.cs
+++ MauiApp1/MauiApp1/IdentityHttpClient.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MauiApp1
+﻿namespace MauiApp1;
+public class IdentityHttpClient : HttpClient
 {
-    internal class IdentityHttpClient
-    {
-    }
 }
