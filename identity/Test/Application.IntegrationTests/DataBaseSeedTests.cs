cc8c148a6c91dbb412e59007c640d7b8eb749628
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-12-14 16:49:34 +0500

    1

---
+++ identity/Test/Application.IntegrationTests/DataBaseSeedTests.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using Identity_Domain.Entities;
+using Identity_Infrastructure.Identity;
+using System.Security.Claims;
+
+
+namespace Application.IntegrationTests;
+
+public class DataBaseSeedTests : BaseTest
+{
+    #region SuperAdmin
+
+    private async Task<ApplicationUser> GetSuperAdmin() =>
+    await GetEntity<ApplicationUser>(s => s.UserName == "SuperAdmin");
+
+    [Test]
+    public async Task SuperAdminRole()
+    {
+        var superAdminRole = await GetEntity<ApplicationRole>(s =>
+            s.NormalizedName == ApplicationClaimValues.SuperAdministrator.ToUpper());
+        superAdminRole.Should().NotBeNull();
+    }
+
+    [Test]
+    public async Task SuperAdminUser()
+    {
+        (await GetSuperAdmin()).Should().NotBeNull();
+    }
+
+    [Test]
+    public async Task SuperAdminClaims()
+    {
+        var superAdmin = await GetSuperAdmin();
+
+        var roleClaim = await GetEntity<ApplicationUserClaim>(s =>
+            s.UserId == superAdmin.Id &&
+            s.ClaimType == ClaimTypes.Role);
+
+        roleClaim.Should().NotBeNull();
+    }
+
+    #endregion
+
+    #region TSRAdmin
+
+    private async Task<ApplicationUser> GetTSRAdmin() =>
+        await GetEntity<ApplicationUser>(s => s.UserName == "TSRAdmin");
+
+    [Test]
+    public async Task TSRAdminRole()
+    {
+        var superAdmin = await GetTSRAdmin();
+        var superAdminRole = await GetEntity<ApplicationRole>(s =>
+            s.NormalizedName == ApplicationClaimValues.Administrator.ToUpper());
+        superAdminRole.Should().NotBeNull();
+
+        var userRole = await GetEntity<ApplicationUserRole>(s =>
+            s.RoleId == superAdminRole.Id &&
+            s.UserId == superAdmin.Id);
+
+        userRole.Should().NotBeNull();
+    }
+
+    [Test]
+    public async Task TSRAdminUser()
+    {
+        (await GetTSRAdmin()).Should().NotBeNull();
+    }
+
+    [Test]
+    public async Task TSRAdminClaims()
+    {
+        var superAdmin = await GetTSRAdmin();
+
+        var roleClaim = await GetEntity<ApplicationUserClaim>(s =>
+            s.UserId == superAdmin.Id &&
+            s.ClaimType == ClaimTypes.Role);
+
+        roleClaim.Should().NotBeNull();
+    }
+
+    #endregion
+}
