c1b92ac0785f29d65b52155ff1c982bea7f78bc4
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-12-16 22:06:16 +0500

    fix LoginPage

--- MauiApp1/MauiApp1/LoginPage.xaml.cs
+++ MauiApp1/MauiApp1/LoginPage.xaml.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using System.Text.Json;
+
 namespace MauiApp1;
 
 public partial class LoginPage : ContentPage
@@ -6,4 +9,27 @@ public partial class LoginPage : ContentPage
 	{
 		InitializeComponent();
 	}
-}
+    private async void OnLoginButtonClicked(object sender, EventArgs e)
+    {
+        var username = usernameEntry.Text;
+        var password = passwordEntry.Text;
+
+        var loginData = new { Username = username, Password = password };
+        var json = JsonSerializer.Serialize(loginData);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync("https://yourapiurl.com/api/auth/login", content);
+
+        if (response.IsSuccessStatusCode)
+        {
+            // Успешный вход
+            var token = await response.Content.ReadAsStringAsync();
+            // Сохраните токен для дальнейшего использования (например, в SecureStorage)
+        }
+        else
+        {
+            // Обработка ошибки
+            var errorMessage = await response.Content.ReadAsStringAsync();
+            await DisplayAlert("Error", errorMessage, "OK");
+        }
+    }
