89fa917eb4d2065561b2cf7fbdc33cc21fe30828
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2025-01-23 22:10:39 +0500

    1

--- MauiApp1/MauiApp1/Services/Auth/AuthService.cs
+++ MauiApp1/MauiApp1/Services/Auth/AuthService.cs
@@ -1,113 +1,113 @@
-﻿//using Identity_Application.Contracts.User.Commands.ChangePassword;
-//using Identity_Application.Contracts.User.Commands.LoginUser;
-//using Identity_Application.Contracts.User.Commands.RegisterUser;
-//using Identity_Application.Contracts.User.Queries.CheckUserDetails;
-//using Identity_Application.Contracts.User.Responses;
-//using MauiApp1.Identity;
-//using System.Net;
-//using System.Net.Http.Json;
+﻿using Identity_Application.Contracts.User.Commands.ChangePassword;
+using Identity_Application.Contracts.User.Commands.LoginUser;
+using Identity_Application.Contracts.User.Commands.RegisterUser;
+using Identity_Application.Contracts.User.Queries.CheckUserDetails;
+using Identity_Application.Contracts.User.Responses;
+using MauiApp1.Identity;
+using MauiApp1.Services.Profile;
+using System.Net;
+using System.Net.Http.Json;
 
-//namespace MauiApp1.Services.Auth
-//{
-//    public class AuthService(IdentityHttpClient identityHttpClient,
-//          AuthenticationStateProvider authenticationStateProvider, NavigationManager navigationManager,
-//          IAltairCABlazorCookieUtil cookieUtil, LayoutService layoutService, IUserProfileService userProfileService)
-//      : IAuthService
-//    {
-//        public async Task<HttpResponseMessage> ChangePassword(ChangePasswordUserCommand command)
-//        {
+namespace MauiApp1.Services.Auth
+{
+    public class AuthService(IdentityHttpClient identityHttpClient,
+          AuthenticationStateProvider authenticationStateProvider, IUserProfileService userProfileService)
+      : IAuthService
+    {
+        public async Task<HttpResponseMessage> ChangePassword(ChangePasswordUserCommand command)
+        {
 
-//            var result = await identityHttpClient.PutAsJsonAsync("Auth/ChangePassword", command);
-//            return result;
-//        }
+            var result = await identityHttpClient.PutAsJsonAsync("Auth/ChangePassword", command);
+            return result;
+        }
 
-//        public async Task<string> LoginUserAsync(LoginUserCommand command, bool newRegister = false)
-//        {
-//            string errorMessage = null;
-//            try
-//            {
-//                var result = await identityHttpClient.PostAsJsonAsync("Auth/login", command);
-//                if (result.IsSuccessStatusCode)
-//                {
-//                    var response = await result.Content.ReadFromJsonAsync<JwtTokenResponse>();
-//                    await cookieUtil.SetValueAsync("authToken", response);
-//                    await authenticationStateProvider.GetAuthenticationStateAsync();
-//                    layoutService.User = await userProfileService.Get();
-//                    if (!newRegister)
-//                        navigationManager.NavigateTo("/");
-//                    return null;
-//                }
+        public async Task<string> LoginUserAsync(LoginUserCommand command, bool newRegister = false)
+        {
+            string errorMessage = null;
+            try
+            {
+                var result = await identityHttpClient.PostAsJsonAsync("Auth/login", command);
+                if (result.IsSuccessStatusCode)
+                {
+                    var response = await result.Content.ReadFromJsonAsync<JwtTokenResponse>();
+                    await cookieUtil.SetValueAsync("authToken", response);
+                    await authenticationStateProvider.GetAuthenticationStateAsync();
+                    layoutService.User = await userProfileService.Get();
+                    if (!newRegister)
+                        navigationManager.NavigateTo("/");
+                    return null;
+                }
 
-//                if (result.StatusCode == HttpStatusCode.Unauthorized)
-//                {
-//                    errorMessage = (await result.Content.ReadFromJsonAsync<CustomProblemDetails>()).Detail;
-//                }
-//            }
-//            catch (HttpRequestException ex)
-//            {
-//                Console.WriteLine(ex);
-//                errorMessage = "Server is not responding, please try later";
-//            }
-//            catch (Exception e)
-//            {
-//                Console.WriteLine(e);
-//                errorMessage = "An error occurred";
-//            }
+                if (result.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    errorMessage = (await result.Content.ReadFromJsonAsync<CustomProblemDetails>()).Detail;
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine(ex);
+                errorMessage = "Server is not responding, please try later";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                errorMessage = "An error occurred";
+            }
 
-//            return errorMessage;
-//        }
+            return errorMessage;
+        }
 
-//        public async Task<string> RegisterUserAsync(RegisterUserCommand command)
-//        {
-//            try
-//            {
-//                command.PhoneNumber = command.PhoneNumber.Trim();
-//                if (command.PhoneNumber.Length == 9) command.PhoneNumber = "+992" + command.PhoneNumber.Trim();
-//                else if (command.PhoneNumber.Length == 12 && command.PhoneNumber[0] != '+') command.PhoneNumber = "+" + command.PhoneNumber;
+        public async Task<string> RegisterUserAsync(RegisterUserCommand command)
+        {
+            try
+            {
+                command.PhoneNumber = command.PhoneNumber.Trim();
+                if (command.PhoneNumber.Length == 9) command.PhoneNumber = "+992" + command.PhoneNumber.Trim();
+                else if (command.PhoneNumber.Length == 12 && command.PhoneNumber[0] != '+') command.PhoneNumber = "+" + command.PhoneNumber;
 
-//                var result = await identityHttpClient.PostAsJsonAsync("Auth/register", command);
-//                if (result.IsSuccessStatusCode)
-//                {
-//                    await LoginUserAsync(new LoginUserCommand()
-//                    {
-//                        Password = command.Password,
-//                        Username = command.Username
-//                    });
-//                    layoutService.User = await userProfileService.Get();
-//                    navigationManager.NavigateTo("/profile");
+                var result = await identityHttpClient.PostAsJsonAsync("Auth/register", command);
+                if (result.IsSuccessStatusCode)
+                {
+                    await LoginUserAsync(new LoginUserCommand()
+                    {
+                        Password = command.Password,
+                        Username = command.Username
+                    });
+                    layoutService.User = await userProfileService.Get();
+                    navigationManager.NavigateTo("/profile");
 
-//                    return "";
-//                }
-//                if (result.StatusCode is not (HttpStatusCode.Unauthorized or HttpStatusCode.BadRequest))
-//                    return "server error, please try again later";
+                    return "";
+                }
+                if (result.StatusCode is not (HttpStatusCode.Unauthorized or HttpStatusCode.BadRequest))
+                    return "server error, please try again later";
 
-//                var response = await result.Content.ReadFromJsonAsync<CustomProblemDetails>();
-//                return response.Detail;
-//            }
-//            catch (HttpRequestException ex)
-//            {
-//                Console.WriteLine(ex);
-//                return "Server is not responding, please try later";
-//            }
-//            catch (Exception e)
-//            {
-//                Console.WriteLine(e);
-//                return "An error occurred";
-//            }
-//        }
+                var response = await result.Content.ReadFromJsonAsync<CustomProblemDetails>();
+                return response.Detail;
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine(ex);
+                return "Server is not responding, please try later";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return "An error occurred";
+            }
+        }
 
-//        public async Task<HttpResponseMessage> CheckUserName(string userName)
-//        {
-//            var result = await identityHttpClient.GetAsync($"User/CheckUserName/{userName}");
-//            return result;
-//        }
+        public async Task<HttpResponseMessage> CheckUserName(string userName)
+        {
+            var result = await identityHttpClient.GetAsync($"User/CheckUserName/{userName}");
+            return result;
+        }
 
-//        public async Task<HttpResponseMessage> CheckUserDetails(CheckUserDetailsQuery query)
-//        {
-//            var result = await identityHttpClient.GetAsync($"User/CheckUserDetails/{query.UserName}/{query.PhoneNumber}/{query.Email}");
-//            return result;
-//        }
+        public async Task<HttpResponseMessage> CheckUserDetails(CheckUserDetailsQuery query)
+        {
+            var result = await identityHttpClient.GetAsync($"User/CheckUserDetails/{query.UserName}/{query.PhoneNumber}/{query.Email}");
+            return result;
+        }
 
 
-//    }
-//}
+    }
+}
